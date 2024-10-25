10843ea4102bd6dfc779e37e96aa19c2d156133f
lenafiedor <lenafiedor@o2.pl>
2024-10-25 19:09:21 +0200

    Start ex-04

---
+++ 01-gamblers-ruin/ex-04.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from simulation import *
+
+
+if __name__ == '__main__':
+
+    # set initial capitals and define probabilities
+    a_initial = 50
+    b_initial = 50
+    p_values = np.arange(0.0, 1.0, 0.1)
